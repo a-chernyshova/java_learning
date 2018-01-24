d674bc46003ac9d952528748f01edc2deacac67e
Anastasiia Chernyshova <anastasiia_chernyshova@epam.com>
2018-01-24 16:00:13 -0500

    request args input if there was no args passed

--- jsonparser/src/main/java/org/learning/parser/App.java
+++ jsonparser/src/main/java/org/learning/parser/App.java
@@ -5,19 +5,29 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
         if (args.length <= 1) {
-            exit("File and node names are required");
+            System.out.println("File and node names are required");
+
+            //lterm
+            //C:\Users\Anastasiia_Chernysho\epam_trainings\final_java_repo\java_learning\jsonparser\src\main\resources\test.json
+            RequestArgs requestArgs = new RequestArgs().invoke();
+            File source = requestArgs.getSource();
+            String nodeName = requestArgs.getNodeName();
+
+            getNodeValue(source, nodeName);
+        } else {
+            System.out.println(args[0]);
+            File source = new File(args[0]);
+            String nodeName = args[1];
+            if (!source.exists()) {
+                exit("File isn't found");
+            }
+            getNodeValue(source, nodeName);
         }
-        System.out.println(args[0]);
-        File source = new File(args[0]);
-        String nodeName = args[1];
-        if (!source.exists()) {
-            exit("File isn't found");
-        }
-        getNodeValue(source, nodeName);
     }
 
     private static void getNodeValue(File source, String nodeName) {
@@ -38,5 +48,27 @@ public class App {
     private static void exit(String s) {
         System.out.println(s);
         System.exit(100);
+    }
+
+    private static class RequestArgs {
+        private File source;
+        private String nodeName;
+
+        public File getSource() {
+            return source;
+        }
+
+        public String getNodeName() {
+            return nodeName;
+        }
+
+        public RequestArgs invoke() {
+            Scanner input = new Scanner(System.in);
+            System.out.println("Input path to file:");
+            source = new File(input.nextLine());
+            System.out.println("Input node name:");
+            nodeName = input.nextLine();
+            return this;
+        }
     }
 }
