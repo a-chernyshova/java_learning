b0239d1430c6b06ab39872c5be0eec37971dea7d
nassykusaka <stasya.aska@gmail.com>
2018-02-02 00:24:30 -0500

    Login to the mail box. Assert, that the login is successful. Create a new mail (fill addressee, subject and body fields). Save the mail as a draft. Verify, that the mail presents in ‘Drafts’ folder. Verify the draft content (addressee, subject and body – should be the same as in 3). Send the mail. Verify, that the mail disappeared from ‘Drafts’ folder. Verify, that the mail is in ‘Sent’ folder. Log off

--- seleniumpractice/src/test/java/learning/AppTest.java
+++ seleniumpractice/src/test/java/learning/AppTest.java
@@ -2,6 +2,7 @@ package learning;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -9,61 +10,86 @@ import org.testng.annotations.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
+import static org.openqa.selenium.support.ui.ExpectedConditions.stalenessOf;
 
 import java.io.File;
 import java.security.Key;
+import java.util.ArrayList;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class AppTest {
 
+    private static String log = "correct_login";
+    private static String pass = "correct_pass";
+    private static String address = "address@gmail.com";
+    private static String subject = "Selenium webdriver";
+    private static String text = "\"The primary new feature in Selenium 2.0 is the integration of the WebDriver API. " +
+            "WebDriver is designed to provide a simpler, more concise programming interface in addition to addressing " +
+            "some limitations in the Selenium-RC API. /nSelenium-WebDriver was developed to better support dynamic " +
+            "web pages where elements of a page may change without the page itself being reloaded. /nWebDriver’s " +
+            "goal is to supply a well-designed object-oriented API that provides improved support for modern " +
+            "advanced web-app testing problems.\"";
     protected WebDriver chromeDriver;
+    protected WebDriverWait wait;
 
     @BeforeClass(alwaysRun = true)
     public void setUp() {
         chromeDriver = new ChromeDriver();
+        wait = new WebDriverWait(chromeDriver, 5);
         //WebDriver firefoxDriver = new FirefoxDriver();
     }
 
-    @Test(enabled = false)
-    public void loginFailTest(){
+    public String authorization(String log, String pass){
         chromeDriver.get("https://www.yandex.ru/");
         WebElement loginDiv = chromeDriver.findElement(By.className("desk-notif"));
-        loginDiv.findElement(By.tagName("input")).sendKeys("nassy.kusaka" + Keys.TAB +"awesome" + Keys.ENTER);
+        loginDiv.findElement(By.tagName("input")).sendKeys(log + Keys.TAB + pass + Keys.ENTER);
         String winTitle = chromeDriver.getTitle();
-        chromeDriver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
-        //Boolean errorMessage = chromeDriver.findElement(By.ByClassName("")).isDisplayed();
+        return winTitle;
+    }
+
+    @Test(enabled = true)
+    public void loginFailTest(){
+        String winTitle = authorization(log, (pass + "123")); //incorrect pass
         Assert.assertEquals(winTitle, "Авторизация");
     }
 
-    @Test //(dependsOnMethods = {"loginFailTest"})
+    @Test(dependsOnMethods = {"loginFailTest"})
     public void loginTest(){
-        chromeDriver.get("https://www.yandex.ru/");
-        WebElement loginDiv = chromeDriver.findElement(By.className("desk-notif"));
-        loginDiv.findElement(By.tagName("input")).sendKeys("nassy.kusaka" + Keys.TAB +"awesome1" + Keys.ENTER);
-        String winTitle = chromeDriver.getTitle();
-        chromeDriver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
+        String winTitle = authorization(log, pass);
         Assert.assertEquals(winTitle, "Yandex.Mail");
+    }
+
+    public String findMailSubject(){
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".mail-MessageSnippet-Content")));
+        WebElement lastMail = chromeDriver.findElement(By.cssSelector(".mail-MessageSnippet-Content"));
+        String mailSubject = lastMail.findElement(By.className("mail-MessageSnippet-Item_subject")).
+                findElement(By.tagName("span")).getAttribute("title");
+        return mailSubject;
+    }
+
+    public String findMailAddress(){
+        WebElement lastMail = chromeDriver.findElement(By.cssSelector(".mail-MessageSnippet-Content"));
+        String email = lastMail.findElement(By.className("mail-MessageSnippet-FromText")).getAttribute("title");
+        return email;
     }
 
     @Test(dependsOnMethods = {"loginTest"})
     public void createDraftTest(){
+        wait.until(ExpectedConditions.elementToBeClickable(By.className("mail-ComposeButton ")));
         chromeDriver.findElement(By.className("mail-ComposeButton ")).click();
-
-        WebDriverWait wait = new WebDriverWait(chromeDriver, 5);
         wait.until(ExpectedConditions.elementToBeClickable(By.className("mail-Compose-Field-Input")));
 
+        //fill out mail form: address, subject, message body
         WebElement currentElement = chromeDriver.switchTo().activeElement();
-        currentElement.sendKeys("anastasiia_chernyshova@epam.com" + Keys.TAB + "Selenium webdriver" +
-                Keys.TAB + "The primary new feature in Selenium 2.0 is the integration of the WebDriver API. WebDriver is designed to provide a simpler, more concise programming interface in addition to addressing some limitations in the Selenium-RC API. Selenium-WebDriver was developed to better support dynamic web pages where elements of a page may change without the page itself being reloaded. WebDriver’s goal is to supply a well-designed object-oriented API that provides improved support for modern advanced web-app testing problems.");
+        currentElement.sendKeys(address + Keys.TAB + subject + Keys.TAB + text);
 
+        //save draft
         chromeDriver.findElement(By.cssSelector(".ns-view-folders a:last-child")).click();
         chromeDriver.findElement(By.className("_nb-modal-popup ")).findElement(By.tagName("button")).click();
-        WebElement lastSaved = chromeDriver.findElement(By.cssSelector(".ns-view-messages-wrap .ns-view-messages-item-wrap"));
-        String title = lastSaved.findElement(By.className("mail-MessageSnippet-Item_subject")).findElement(By.tagName("span")).getAttribute("title");
-        String email = lastSaved.findElement(By.className("mail-MessageSnippet-FromText")).getAttribute("title");
-        Assert.assertEquals(title, "Selenium webdriver");
-        Assert.assertEquals(email, "anastasiia_chernyshova@epam.com");
+
+        Assert.assertEquals(findMailSubject(), subject);
+        Assert.assertEquals(findMailAddress(), address);
     }
 
     @Test(dependsOnMethods = {"createDraftTest"})
@@ -72,11 +98,31 @@ public class AppTest {
         WebElement lastSaved = chromeDriver.findElement(By.cssSelector(".ns-view-messages-wrap .ns-view-messages-item-wrap"));
         lastSaved.findElement(By.className("js-message-snippet-sender")).click();
         chromeDriver.findElement(By.className("ns-view-compose-action-buttons")).click();
-        //chromeDriver.findElement(By.className("mail-Done-Title")).getAttribute("");
-        // TODO: assert if mail was sent
+        String success = chromeDriver.findElement(By.className("mail-Done-Title")).getText();
+        Assert.assertEquals(success, "Message sent successfully.");
     }
 
     @Test(dependsOnMethods = {"sendDraftTest"})
+    public void checkDraftTest(){
+        chromeDriver.findElement(By.cssSelector(".ns-view-folders a:last-child")).click();
+        try{
+            chromeDriver.findElements(By.className("ns-view-messages-empty"));
+        }catch (NoSuchElementException e){
+            Assert.assertTrue(false);
+        };
+    }
+
+    @Test(dependsOnMethods = {"sendDraftTest"})
+    public void checkSentTest(){
+        chromeDriver.findElement(By.linkText("Sent")).click();
+        WebElement lastSaved = chromeDriver.findElement(By.cssSelector(".ns-view-messages-wrap .ns-view-messages-item-wrap"));
+        String title = lastSaved.findElement(By.className("mail-MessageSnippet-Item_subject")).findElement(By.tagName("span")).getAttribute("title");
+        String email = lastSaved.findElement(By.className("mail-MessageSnippet-FromText")).getAttribute("title");
+        Assert.assertEquals(title, subject);
+        Assert.assertEquals(email, address);
+    }
+
+    @Test(dependsOnMethods = {"checkSentTest"})
     public void logoutTest(){
         chromeDriver.findElement(By.className("mail-User-Name")).click();
         chromeDriver.findElement(By.linkText("Log out")).click();
