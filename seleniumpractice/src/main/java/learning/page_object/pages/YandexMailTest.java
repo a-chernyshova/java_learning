857a409608166c3895602efe7c96f200ef9751fa
nassykusaka <stasya.aska@gmail.com>
2018-02-07 00:20:17 -0500

    added data provider

--- seleniumpractice/src/main/java/learning/page_object/pages/YandexMailTest.java
+++ seleniumpractice/src/main/java/learning/page_object/pages/YandexMailTest.java
@@ -2,24 +2,17 @@ package learning.page_object.pages;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.*;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import static org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable;
-import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
-import static org.openqa.selenium.support.ui.ExpectedConditions.stalenessOf;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import org.testng.annotations.AfterClass;
 import java.util.concurrent.TimeUnit;
 
 public class YandexMailTest {
     private WebDriver driver;
-    private WebDriverWait wait;
-    private static final String[] LOG_PASS = {"address", "password"};
-    private static final String[] INCORRECT_LOG_PASS = {"address", "incorrect"};
-    private static final String[] EMAIL_DATA = {"test@gmail.com", "Selenium webdriver",
+    private static final String[] EMAIL_DATA = {"stasya.aska@gmail.com", "Selenium webdriver",
             "The primary new feature in Selenium 2.0 is the integration of the WebDriver API. " +
             "WebDriver is designed to provide a simpler, more concise programming interface in addition to addressing " +
             "some limitations in the Selenium-RC API. \nSelenium-WebDriver was developed to better support dynamic " +
@@ -30,37 +23,41 @@ public class YandexMailTest {
     @BeforeClass(description="Start browser")
     public void setUp() {
         driver = new ChromeDriver();
-        wait = new WebDriverWait(driver, 5);
         driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
     }
 
-    private String authorization(String[] LOG_PASS){
+    private String authorization(String log, String pass){
         HomePage homePage = new HomePage(driver);
         homePage.open();
 
         LoginPage logForm = new LoginPage(driver);
-        logForm.fillLoginCredentials(LOG_PASS);
-        String winTitle = driver.getTitle();
-        return winTitle;
+        logForm.fillLoginCredentials(log, pass);
+        return driver.getTitle();
     }
 
-    @Test(enabled = true)
-    public void loginFailTest(){
-        String winTitle = authorization(INCORRECT_LOG_PASS); //incorrect pass
+    @DataProvider(name="loginCredentials")
+    public Object[][] loginCredentials(){
+        return new Object[][]{
+                {"nassy.kusaka", "letstestit"}
+        };
+    }
+
+    @Test(enabled = true, dataProvider = "loginCredentials",description="check logging in with incorrect credentials")
+    public void loginFailTest(String log, String pass){
+        String winTitle = authorization(log, (pass + "123")); //incorrect pass
         Assert.assertEquals(winTitle, "Авторизация");
     }
 
-    @Test(dependsOnMethods = {"loginFailTest"})
-    public void loginTest(){
-        String winTitle = authorization(LOG_PASS);
+    @Test(dependsOnMethods = {"loginFailTest"}, dataProvider = "loginCredentials")
+    public void loginTest(String log, String pass){
+        String winTitle = authorization(log, pass);
         Assert.assertEquals(winTitle, "Yandex.Mail");
     }
 
     public String findMailSubject(){
         LettersContainer mailList = new LettersContainer(driver);
-        wait.until(ExpectedConditions.elementToBeClickable(mailList.getLastMessageLocator()));
         String mailSubject = mailList.getLastMessageSubject();
         return mailSubject;
     }
@@ -74,9 +71,7 @@ public class YandexMailTest {
     @Test(dependsOnMethods = {"loginTest"})
     public void createDraftTest(){
         Menu mailBoxMenu = new Menu(driver);
-        wait.until(elementToBeClickable(mailBoxMenu.getCreateButtonLocator()));
-        NewLetter newLetterForm = mailBoxMenu.openNewMailForm();
-        wait.until(elementToBeClickable(newLetterForm.getInputLocator()));
+        learning.page_object.pages.NewLetter newLetterForm = mailBoxMenu.openNewMailForm();
         newLetterForm.fillNewLetterForm(EMAIL_DATA);
         mailBoxMenu.openDraftsFolder();
         newLetterForm.saveDraft();
@@ -88,8 +83,8 @@ public class YandexMailTest {
     @Test(dependsOnMethods = {"createDraftTest"})
     public void sendDraftTest(){
         LettersContainer drafts = new LettersContainer(driver);
-        NewLetter currentLetter = drafts.openLastMessage();
-        SendResultPage result = currentLetter.sendLetter();
+        learning.page_object.pages.NewLetter currentLetter = drafts.openLastMessage();
+        learning.page_object.pages.SendResultPage result = currentLetter.sendLetter();
         Assert.assertEquals(result.getStatus(), "Message sent successfully.");
     }
 
